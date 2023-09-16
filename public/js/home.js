2f2f780050ab25857d07d8de712e4b8050dad3d8
Fabian Barua <baruafabian@gmail.com>
2023-09-16 11:20:58 -0400

    css

--- public/js/home.js
+++ public/js/home.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const barButton = document.getElementById("bar");
     const submenu = document.getElementById("submenu");
 
-
     if (barButton && submenu) {
         barButton.addEventListener("click", function (event) {
+            event.stopPropagation(); 
             toggleNav(submenu);
         });
 
         document.addEventListener("click", function (event) {
             if (!submenu.contains(event.target)) {
-                submenu.classList.remove("active"); 
+                submenu.classList.remove("active");
             }
         });
     }
