72af673f6a94d52ea4597949d120c0a20f6810eb
Fabian Barua <baruafabian@gmail.com>
2023-09-23 10:37:25 -0400

    design

--- controller/proyectos.js
+++ controller/proyectos.js
@@ -3,3 +3,14 @@ exports.home = (req, res) =>{
         pageName : 'Home'
         });
 }
+exports.codigos = (req, res) =>{
+        res.render('codigos', {
+        pageName : 'Codes'
+        });
+}
+
+exports.code = (req, res) =>{
+        res.render('code', {
+        pageName : 'Watch'
+});
+}
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,8 @@ const controller = require('../controller/proyectos.js')
 module.exports = function(){
 
     router.get('/', controller.home);
-
+    router.get('/c', controller.codigos)
+    router.get('/c/:codigo', controller.code)
     return router;
 }
 
