825fcf88003450eb2e6dff0a4f5a69eabddbdbe6
Fabian Barua <baruafabian@gmail.com>
2023-09-30 09:35:32 -0400

    back

--- controller/proyectos.js
+++ controller/proyectos.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require("sequelize");
 const Codes = require('../models/codes')
-
+const fs = require('fs');
+const path = require('path');
 
 exports.home = (req, res) =>{
         res.render('home', {
@@ -10,9 +11,20 @@ exports.home = (req, res) =>{
 
 exports.codes = async(req, res) =>{
         const codes  = await Codes.findAll()
+
+        const images = codes.map((item) => {
+        const languages = item.language.split('-');
+        const rowImages = languages.map((language) => {
+                const imagePath = path.join('public', 'img', 'SVG', `${language.toLowerCase()}.svg`);
+                return fs.existsSync(imagePath) ? 1 : 0;
+        });
+        return rowImages;
+        });
+
         res.render('codigos', {
         pageName : 'Codes',
-        codes: codes
+        codes: codes,
+        images: images
         });
 }
 
